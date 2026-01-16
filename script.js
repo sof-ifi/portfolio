e9314cee4e301812f5ce1f3b62b55f62c53f6b3a
sof-ifi <saysofya@gmail.com>
2026-01-16 14:02:42 +0000

    Update script.js

--- script.js
+++ script.js
@@ -59,16 +59,22 @@ const PriceCalculator = (function() {
    * @param {boolean} show - Whether to display the image
    */
   function updateImage(elementId, src, show = true) {
+    const img = getElement(elementId);
     const img = get
       Element(elementId);
     if (img) {
       img.src = src;
-      img.style.display = show ? "block" : "none"; 
+      img.style.display = show ? "block" : "none";
+        if (show) {
+          img.style.display = "block";
+        } 
+        else {
+          img.style.display = "none";
+        }
     } else {
       console.warn(`Image element not found: ${elementId}`);
     }
-  }
-  
+    
   /**
    * Hides an image element
    * @private
